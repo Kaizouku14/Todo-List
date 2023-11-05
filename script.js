c31d3086c3ba73ddf4047d003c46b7a8b428bc67
Kaizouku14 <124888425+Kaizouku14@users.noreply.github.com>
2023-11-05 18:54:06 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let items = [];
+
+renderItems();
+
+function renderItems(){
+  let lists = '', i = 0;
+
+  items.forEach((value) =>{
+    const {todo,dueDate} = value;
+    const html = `<div class ="list">
+                    <input type="checkbox" class="accept" data-todo ="todo-${i++}">
+                    <div class="todo" data-todo="todo-${i++}">${todo}</div> 
+                    <div class="date">${dueDate}</div>
+                    <button class="delete-button js-delete">x</button>
+                  </div>`;
+    lists += html;
+  });
+
+  document.querySelector('.List-Todo').innerHTML = lists;
+
+  document.querySelectorAll('.js-delete').forEach((delete_button) =>{
+      delete_button.addEventListener('click', (index) =>{
+          items.splice(index,1);
+          renderItems();
+      });
+  });
+ 
+  document.querySelectorAll('.accept').forEach((checkBox) => {
+    checkBox.addEventListener('click', () => {
+      const todoElement = checkBox.parentElement.querySelector('.todo');
+  
+      if (checkBox.checked) 
+        todoElement.style.textDecoration = 'line-through';
+      else 
+        todoElement.style.textDecoration = '';
+    });
+  });
+}
+
+ document.querySelector('.js-add').addEventListener('click', () =>{
+    const list = document.querySelector('.js-text');
+    const date = document.querySelector('.js-due-date');
+    const todo = list.value;
+    const dueDate = date.value;
+    
+    if(list.value !== '' && date.value !== ''){
+        items.push({todo,dueDate});
+        list.value = '';
+        date.value = '';
+        renderItems();
+    }
+ });
+
